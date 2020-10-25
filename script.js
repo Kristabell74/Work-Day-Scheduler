c04c2a1b3e081a79539c327550c894f6cb424cad
Kristabell Johnson <bell77iam@gmail.com>
2020-10-25 17:18:38 -0600

    linking pages together, rows and containers established on html

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ $("#currentDay").text(todaysDate.format('MMMM Do YYYY, h:mm:ss a'));
 
 console.log("todays date")
 
-//objects to sore the work locally
+//objects with variables to put data in order to sort the work locally
 var work = {
-    "8": [], "9": [], "10": [], "11": [], "12": [], "01": [], "02": [], "03": [], "04": [], "05": []
+    "8:00 am": [], "9:00 am": [], "10:00 am": [], "11:00 am": [], "12:00 pm": [], "01:00 pm": [], "02:00 pm": [], "03:00 pm": [], "04:00 pm": [], "05:00 pm": []
 };
 
 //add work to local storage
@@ -15,20 +15,22 @@ var setWork = function () {
     localStorage.setItem('work', JSON.stringify(work));
 }
 
-//get work from local storage
+//persist the data to local storage
 var getWork = function () {
     console.log('getWork')
     var fullWork = JSON.parse; (localStorage.getItem('work'));
     if (fullWork) {
         work = fullWork
 
+        console.log("getWork")
 
         //for each row keep the value entered
-        $.each(work, function (hour, work) {
-            console.log('work')
-            var hourDiv = $()
+        $.each(work, (hour, work) => {
+            var hourDiv = $();
+            console.log("hourDiv");
             createWork(work, hourDiv);
         })
+
         //check the times
         auditWork()
 
@@ -38,7 +40,7 @@ var getWork = function () {
 //put  and keep work in the correct hour
 var createWork = function (workText, hourDiv) {
 
-    var workDiv = workDiv.find('.task');
+    var workDiv = hourDiv.find('.work');
     var workP = $('<p>')
         .addClass('description')
         .text(workText)
@@ -46,27 +48,49 @@ var createWork = function (workText, hourDiv) {
 
 }
 
-//update background of each row depending on time
+//update background  color of each row depending on time
 var auditWork = function () {
-console.log ("auditWork")
+    console.log("auditWork")
     var curentHour = moment().hour();
     $('.work-info').each(function () {
-        var elementHour = parseInt($(this).attr('id'));
+        var elementHour = parseInt($(this).attr(''));
 
-        //keeps the color greyed-out "its all in the past baby"
+        //keeps the color greyed-out - "its all in the past baby"
         if (elementHour < currentHour) {
             $(this).removeClass(['present', 'future']).addClass('past');
         }
-        //keeps the color red for "stop"
+        //keeps the color - red for "stop"
         else if (elementHour === currentHour) {
             $(this).removeClass()(['past', 'future']).addClass('present');
         }
-        //keeps the color Green for "go"
+        //keeps the color - Green for "go"
         else {
             $(this).removeClass(['past', 'present']).addClass('future');
+
+            console.log("elementHour: ", elementHour)
         }
     })
 };
+
+//local storage for when text is entered on the calendar
+
+// var replaceTextarea = function (textareaElement) {
+
+//     //Pulls the elements in order to put time and notations together
+//     var workInfo = textareaElement.closest(".work-info");
+//     var textArea = workInfo.find('textarea');
+
+//     //get the time and notations together
+//     var time = workInfo.attr('');
+//     var work = textArea.vail().trim();
+
+//     //stores the data in the system
+//     work[time] = [text];
+//     setWork();
+
+//     //replace the textarea element with a p 
+//     createWork(work, workInfo);
+// }
 
 
 
