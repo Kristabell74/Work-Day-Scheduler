5ed7bb50ce30b627544da1642d538bf17dc28abd
Kristabell Johnson <bell77iam@gmail.com>
2020-10-25 23:17:22 -0600

    added more html and javascript trying to get them to connect better

--- script.js
+++ script.js
@@ -12,17 +12,15 @@ var work = {
 //add work to local storage
 var setWork = function () {
 
-    localStorage.setItem('work', JSON.stringify(work));
+    localStorage.setItem("work", JSON.stringify(work));
 }
 
 //persist the data to local storage
 var getWork = function () {
 
-    var fullWork = JSON.parse; (localStorage.getItem('work'));
+    var fullWork = JSON.parse; (localStorage.getItem("work"));
     if (fullWork) {
         work = fullWork
-
-        console.log("getWork")
 
         //for each row keep the value entered
         $.each(work, (_hour, work) => {
@@ -40,7 +38,7 @@ var getWork = function () {
 //put  and keep work in the correct hour
 var createWork = function (workText, hourDiv) {
 
-    var workDiv = hourDiv.find('.work');
+    var workDiv = hourDiv.find(".work");
     var workP = $('<p>')
         .addClass('description')
         .text(workText)
@@ -50,10 +48,10 @@ var createWork = function (workText, hourDiv) {
 
 //update background  color of each row depending on time
 var auditWork = function () {
-    console.log("auditWork")
+
     var currentHour = moment().hour();
     $('.work-info').each(function () {
-        var elementHour = parseInt($(this).attr(''));
+        var elementHour = parseInt($(this).attribute(''));
 
         //keeps the color greyed-out - "its all in the past baby"
         if (elementHour < currentHour) {
@@ -72,5 +70,48 @@ var auditWork = function () {
     })
 }
 
+
+//when click the save button
+
+$("work").click(function () {
+
+    //saves the work if the button has been clicked 
+    $("textarea").each(function () {
+        replaceTextarea = ($(this));
+
+    })
+
+    //this is keeping information with the time at the click of the button if it has changed
+    var time = $(this).closest("work").attribute("id");
+    if (parseInt(time) >= moment().hour()) {
+        //this is the click feature for the button 
+        var text = $(this).text();
+        var textInput = $("<textarea>")
+            .addClass("container4")
+            .value(text);
+
+        //click the button to save any input
+        $(this).html(textInput);
+        textInput.trigger("click");
+    }
+
+})
+//save button click it in order to save information
+
+$("saveBtn").click(function () {
+    replaceTextArea($(this));
+
+    //keep track of time 
+    timeToHour = 86400 - today.seconds();
+
+    //when time is past change the hour on calendar that is on the hour and in the future
+    setTimeout(function () {
+        setInterval(auditWork, 86400)
+    }, timeToHour);
+
+    //get the current work and the future work and calendar all 
+    getWork()
+
+})
 
 
