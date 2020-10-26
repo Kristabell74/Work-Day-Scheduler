70b6d225b81a526cd01fd9d1c8e3d0046d84a1fb
Kristabell Johnson <bell77iam@gmail.com>
2020-10-26 14:46:39 -0600

    trying to get Javascript to connect properly to html

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
+
 //date and time
 var todaysDate = moment();
 $("#currentDay").text(todaysDate.format('MMMM Do YYYY, h:mm:ss a'));
-
-console.log("todays date")
 
 //objects with variables to put data in order to sort the work locally
 var work = {
@@ -15,103 +14,58 @@ var setWork = function () {
     localStorage.setItem("work", JSON.stringify(work));
 }
 
-//persist the data to local storage
-var getWork = function () {
 
-    var fullWork = JSON.parse; (localStorage.getItem("work"));
-    if (fullWork) {
-        work = fullWork
+// this adds the value of the textarea to the w,ork variable
+var saveBtn = input.getElementById("work");
+localStorage.setItem("work", input.value());
 
-        //for each row keep the value entered
-        $.each(work, (_hour, work) => {
-            var hourDiv = $();
+//This tells the computer to save the work when the save button is clicked
+document.getElementById("input").addEventListener("click", function () {
 
-            createWork(work, hourDiv);
-        })
+    //save the current hour and the text area together
+    var user = document.getElementById(".current hour", ".input-area", Json.stringify(work)).value;
+    console.log(currentTime)
+    //local storage of the user 
+    localStorage.setItem("user", user);
 
-        //check the times
-        auditWork()
-
-    }
-}
-
-//put  and keep work in the correct hour
-var createWork = function (workText, hourDiv) {
-
-    var workDiv = hourDiv.find(".work");
-    var workP = $('<p>')
-        .addClass('description')
-        .text(workText)
-    workDiv.html(workP);
-
-}
-
-//update background  color of each row depending on time
-var auditWork = function () {
-
-    var currentHour = moment().hour();
-    $('.work-info').each(function () {
-        var elementHour = parseInt($(this).attribute(''));
-
-        //keeps the color greyed-out - "its all in the past baby"
-        if (elementHour < currentHour) {
-            $(this).removeClass(['present', 'future']).addClass('past');
-        }
-        //keeps the color - red for "stop"
-        else if (elementHour === currentHour) {
-            $(this).removeClass()(['past', 'future']).addClass('present');
-        }
-        //keeps the color - Green for "go"
-        else {
-            $(this).removeClass(['past', 'present']).addClass('future');
-
-            console.log("elementHour: ", elementHour)
-        }
-    })
-}
+}, false);
 
 
-//when click the save button
 
-$("work").click(function () {
 
-    //saves the work if the button has been clicked 
-    $("textarea").each(function () {
-        replaceTextarea = ($(this));
 
-    })
 
-    //this is keeping information with the time at the click of the button if it has changed
-    var time = $(this).closest("work").attribute("id");
-    if (parseInt(time) >= moment().hour()) {
-        //this is the click feature for the button 
-        var text = $(this).text();
-        var textInput = $("<textarea>")
-            .addClass("container4")
-            .value(text);
 
-        //click the button to save any input
-        $(this).html(textInput);
-        textInput.trigger("click");
-    }
 
-})
-//save button click it in order to save information
 
-$("saveBtn").click(function () {
-    replaceTextArea($(this));
+// //when click the save button
 
-    //keep track of time 
-    timeToHour = 86400 - today.seconds();
+// $("work").click(function () {
 
-    //when time is past change the hour on calendar that is on the hour and in the future
-    setTimeout(function () {
-        setInterval(auditWork, 86400)
-    }, timeToHour);
+//     //saves the work if the button has been clicked 
+//     $("textarea").each(function () {
+//         replaceTextarea = ($(this));
 
-    //get the current work and the future work and calendar all 
-    getWork()
+//     })
 
-})
+//     //this is keeping information with the time at the click of the button if it has changed
+//     var time = $(this).closest("work").attribute("id");
+//     if (parseInt(time) >= moment().hour()) {
+//         //this is the click feature for the button 
+//  
+
+//         //click the button to save any input
+
+// //save button click it in order to save information
+
+
+
+//     //keep track of time 
+//     //when time is past change the hour on calendar that is on the hour and in the future
+
+//     //get the current work and the future work and calendar all 
+
+
+
 
 
---
+++ workdaystuff.js
@@ -0,0 +1,28 @@
+// var past = pastDate();
+// var present = presentDate();
+// var future = futureDate();
+
+// console.log(pastDate)
+
+
+// //add work to local storage
+// var setWork = function () {
+//     //localStorage = (key value)
+//     localStorage.setItem("work", JSON.stringify(work));
+// }
+
+// // this adds the value of the textarea to the work variable
+// var input = saveBtn.getElementById.innerHTML("textarea");
+// localStorage.setItem("work", input.value());
+
+// //This tells the computer to save the work when the save button is clicked
+// document.getElementById.innerHTML("savebtn").addEventListener("click", function () {
+
+//     //save the current hour and the text area together
+//     var user = document.getElementById(".current hour", ".textarea", Json.stringify(work)).value;
+
+//     //local storage of the user 
+//     localStorage.setItem("user", user);
+
+// }, false);
+
